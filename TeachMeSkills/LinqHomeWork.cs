39a940d763ec659dc53ab3492151307d45bc64c4
Ekaterina Siniak <e.siniak@defin.by>
2024-03-25 11:07:43 +0300

    added unit tests

--- TeachMeSkills/LinqHomeWork.cs
+++ TeachMeSkills/LinqHomeWork.cs
@@ -1,113 +1,145 @@
-﻿using TeachMeSkills;
+﻿namespace TeachMeSkills;
 
-void OutputResult<T>(IEnumerable<T> list)
+public static class LinqHomeWork
 {
-    foreach (var num in list)
+    static void OutputResult<T>(IEnumerable<T> list)
     {
-        Console.WriteLine(num);
-    }
-}
-
-void GetPositiveTwoDigitNumbers()
-{
-    var list = new List<int>() {11, -21, 64, 59, 11, 0, -7, 53, -31, 1};
-
-    var result = list.Where(t => t >= 10 && t <= 99).Order();
-
-    OutputResult(result);
-}
-
-void GetEvenNumbers()
-{
-    var list = new List<int> { 11, 23, 60, 57, 62, 17, 42, 34, 123 };
-
-    var result = list.Where(t => t % 2 == 0);
-
-    OutputResult(result);
-}
-
-void Reverse()
-{
-    var word = "automation";
-
-    Console.WriteLine(word.Reverse().ToArray());
-}
-
-void ChangeFirstLetter()
-{
-    var names = new List<string>()
-    {
-        "Alexa",
-        "Kirill",
-        "Jonas",
-        "Alina",
-        "Andrey",
-        "Danila",
-        "Nick",
-        "Amer",
-        "Chastity",
-        "Kristina",
-        "Amelia"
-    };
-
-    var namesStartsWithA = names.Where(n => n.StartsWith("A"));
-    var namesStartsWithI = new List<string>();
-
-    foreach (var name in namesStartsWithA)
-    {
-        var replace = name.Replace("A", "I");
-        namesStartsWithI.Add(replace);
+        foreach (var num in list)
+        {
+            Console.WriteLine(num);
+        }
     }
 
-    OutputResult(namesStartsWithI);
-}
-
-void UnionListsOfNames()
-{
-    var firstList = new List<string>() { "Igor", "Vasya", "Alex" };
-    var secondList = new List<string>() { "Nikita", "Yura", "Sergey" };
-
-    var names = firstList.Union(secondList).Where(n => n.ElementAt(n.Length - 1) !=  'a');
-
-    var reversedNames = names.Reverse();
-
-    OutputResult(reversedNames);
-}
-
-void SortNamesByLength()
-{
-    var names = new List<string>() { "Brenda", "Kristina", "Anna", "Nik", "Hugo", "Alice", "Selena" };
-
-    var sortedNames = names.OrderBy(n => n.Length);
-
-    var countOfSymbolsList = new List<int>();
-
-    foreach (var name in sortedNames)
+    public static IEnumerable<int> GetPositiveTwoDigitNumbers()
     {
-        countOfSymbolsList.Add(name.Length);
+        var list = new List<int>() { 11, -21, 64, 59, 11, 0, -7, 53, -31, 1 };
+
+        var result = list.Where(t => t >= 10 && t <= 99).Order();
+
+        OutputResult(result);
+
+        return result;
     }
 
-    OutputResult(countOfSymbolsList);
-}
-
-void FilterProductsListByPrice()
-{
-    var productsList = new List<Product>()
+    public static IEnumerable<int> GetEvenNumbers()
     {
-        new Product("Milk", 5422), new Product("Bread", 1233), new Product("Cookies", 694), new Product("Water", 10)
-    };
+        var list = new List<int> { 11, 23, 60, 57, 62, 17, 42, 34, 123 };
 
-    var result = new List<string>();
+        var result = list.Where(t => t % 2 == 0);
 
-    var resultProductsList = productsList.Where(p => p.Price > 1000);
+        OutputResult(result);
 
-    foreach (var product in resultProductsList)
-    {
-        result.Add(product.Name);
+        return result;
     }
 
-    OutputResult(result);
+    public static char[] Reverse()
+    {
+        var word = "automation";
 
+        var result = word.Reverse().ToArray();
+        Console.WriteLine(result);
+
+        return result;
+    }
+
+    public static IEnumerable<string> ChangeFirstLetter()
+    {
+        var names = new List<string>()
+        {
+            "Alexa",
+            "Kirill",
+            "Jonas",
+            "Alina",
+            "Andrey",
+            "Danila",
+            "Nick",
+            "Amer",
+            "Chastity",
+            "Kristina",
+            "Amelia"
+        };
+
+        var namesStartsWithA = names.Where(n => n.StartsWith("A"));
+        var namesStartsWithI = new List<string>();
+
+        foreach (var name in namesStartsWithA)
+        {
+            var replace = name.Replace("A", "I");
+            namesStartsWithI.Add(replace);
+        }
+
+        OutputResult(namesStartsWithI);
+
+        return namesStartsWithI;
+    }
+
+    public static IEnumerable<string> UnionListsOfNames()
+    {
+        var firstList = new List<string>() { "Igor", "Vasya", "Alex" };
+        var secondList = new List<string>() { "Nikita", "Yura", "Sergey" };
+
+        var names = firstList.Union(secondList).Where(n => n.ElementAt(n.Length - 1) != 'a');
+
+        var reversedNames = names.Reverse();
+
+        OutputResult(reversedNames);
+
+        return reversedNames;
+    }
+
+    public static IEnumerable<int> SortNamesByLength()
+    {
+        var names = new List<string>() { "Brenda", "Kristina", "Anna", "Nik", "Hugo", "Alice", "Selena" };
+
+        var sortedNames = names.OrderBy(n => n.Length);
+
+        var countOfSymbolsList = new List<int>();
+
+        foreach (var name in sortedNames)
+        {
+            countOfSymbolsList.Add(name.Length);
+        }
+
+        OutputResult(countOfSymbolsList);
+
+        return countOfSymbolsList;
+    }
+
+    public static IEnumerable<string> FilterProductsListByPrice()
+    {
+        var productsList = new List<Product>()
+        {
+            new("Milk", 5422), new("Bread", 1233), new("Cookies", 694), new("Water", 10)
+        };
+
+        var result = new List<string>();
+
+        var resultProductsList = productsList.Where(p => p.Price > 1000);
+
+        foreach (var product in resultProductsList)
+        {
+            result.Add(product.Name);
+        }
+
+        OutputResult(result);
+        return result;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Извлечение положительных двузначных чисел из последовательности:");
+        GetPositiveTwoDigitNumbers();
+        Console.WriteLine("Выборка чётных чисел из последовательности:");
+        GetEvenNumbers();
+        Console.WriteLine("Вывод слова в обратном порядке:");
+        Reverse();
+        Console.WriteLine("Изменение первой буквы в именах:");
+        ChangeFirstLetter();
+        Console.WriteLine("Объединение списков имен и фильтрация по предпоследней букве:");
+        UnionListsOfNames();
+        Console.WriteLine("Сортировка имён по длине:");
+        SortNamesByLength();
+        Console.WriteLine("Фильтрация списка продуктов по цене:");
+        FilterProductsListByPrice();
+    }
 }
-
-FilterProductsListByPrice();
