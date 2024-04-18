defe7ef0a5d6646f066d1d66b9c300071025b215
xthene <yekaterinasiniack@gmail.com>
2024-04-18 12:19:15 +0300

    Locators fixef

--- TeachMeSkills/InventoryPage.cs
+++ TeachMeSkills/InventoryPage.cs
@@ -6,7 +6,8 @@ namespace TeachMeSkills
     {
         private readonly By backpackLink = By.LinkText("Sauce Labs Backpack");
         private readonly By bikeLightLink = By.PartialLinkText("Bike Light");
-        private readonly By tshirtLink = By.XPath("//button[@id='add-to-cart-sauce-labs-bolt-t-shirt']/ancestor::div[@class='inventory_item_label']/child::a");
+        private readonly By tshirtLink = By.XPath("//div[contains(text(), 'T-Shirt')]/parent::a");
+        private readonly By tshirtDescriptionDiv = By.XPath("//button[@id='add-to-cart-sauce-labs-bolt-t-shirt']/ancestor::div[2]");
         private readonly By addToCartButtons = By.XPath("//div[@class='inventory_item']/descendant::button");
         private readonly By priceDivs = By.XPath("//div[@class='pricebar']/child::div[@class='inventory_item_price']");
         private readonly By backpackPriceBar = By.XPath("//button[@id='add-to-cart-sauce-labs-backpack']/parent::div");
