2c8cc866adcc23a86bb4aa11b4a82630b130e077
xthene <yekaterinasiniack@gmail.com>
2024-05-12 12:14:38 +0300

    changed BaseTest

--- TestRail/Test/BaseTest.cs
+++ TestRail/Test/BaseTest.cs
@@ -11,13 +11,6 @@ namespace TestRail.Test
     {
         public IWebDriver Driver { get; set; }
 
-        //public LoginPage LoginPage { get; set; }
-        //public DashboardPage DashboardPage { get; set; }
-        //public AddProjectPage AddProjectPage { get; set; }
-        //public AdministrationPage AdministrationPage { get; set; }
-        //public ProjectsPage ProjectsPage { get; set; }
-        //public OverviewProjectPage OverviewProjectPage { get; set; }
-
         public NavigationStep NavigationStep { get; set; }
         public UserStep UserStep { get; set; }
 
@@ -31,12 +24,6 @@ namespace TestRail.Test
         public void Setup()
         {
             Driver = new Browser().Driver;
-            //LoginPage = new LoginPage(Driver);
-            //DashboardPage = new DashboardPage(Driver);
-            //AddProjectPage = new AddProjectPage(Driver);
-            //AdministrationPage = new AdministrationPage(Driver);
-            //ProjectsPage = new ProjectsPage(Driver, true);
-            //OverviewProjectPage = new OverviewProjectPage(Driver);
 
             NavigationStep = new NavigationStep(Driver);
             UserStep = new UserStep(Driver);
